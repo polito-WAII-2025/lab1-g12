d50715606e6e3580995fbac2e1c2c1a7f92be03c
Lorenzo Gaetani s333048 <s333048@studenti.polito.it>
2025-03-18 17:46:28 +0100

    RouteAnalyzer module

---
+++ RouteAnalyzer/src/test/kotlin/org/example/routeanalyzer/RouteAnalyzerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.routeanalyzer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RouteAnalyzerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
