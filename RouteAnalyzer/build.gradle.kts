4912b4d8097fbd647fe3fa38b46eef387e7090f6
MARCO AMMIRATI s300269 <s300269@studenti.polito.it>
2025-03-27 15:13:54 +0100

    Docker

---
+++ RouteAnalyzer/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("org.example.MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "org.example.MainKt"
+        )
+    }
+}
